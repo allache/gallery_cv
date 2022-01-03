66378b163217f768df2bd9b087670f2e6fc25cc8
allache abdelmalek <allacheabdelmalek@gmail.com>
2022-01-03 12:38:42 -0500

    put the name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './gallery.css';
 function App() {
   return (
   <div className="background">
+   <h1 style={{textAlign: 'center'}}>allache abdelmalek </h1>
  <h1 style={{textAlign: 'center'}}>Gallery of cv exemples "😎" </h1>
  <h2 style={{textAlign: 'center'}}>All these persones is just AI face not real</h2>
  
