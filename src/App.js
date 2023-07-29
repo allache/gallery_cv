3b74745e441b29d2021ee69fd3931160c7f5316d
allache abdelmalek <allacheabdelmalek@gmail.com>
2023-07-29 05:17:39 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
   <div className="background">
    <h1 style={{textAlign: 'center'}}>allache abdelmalek </h1>
- <h1 style={{textAlign: 'center'}}>Gallery of cv exemples "😎" </h1>
- <h2 style={{textAlign: 'center'}}>All these persones is just AI face not real</h2>
+ <h1 style={{textAlign: 'center'}}>Gallery of my work and deploma  "😎" </h1>
  
 
   <Gallery/>
